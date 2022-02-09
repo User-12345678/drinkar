94f6f94ddc8ad2af42ad88866c91a5672199e22f
Gudslamm <davidkloth@yahoo.se>
2022-02-09 16:55:44 +0100

    first commit

---
+++ src/features/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React, {useState, useEffect} from 'react'
+import "./Header.css"
+
+export default function Header() {
+
+    return (
+        <div className='header'>
+            <h1> DAVIDs DRINKAR </h1>
+
+            <div className='knapp'>
+            <button>Lägg till drink</button>
+            </div>
+            
+        </div>
+    )
+        
+    
+
+}
---
+++ src/features/MainPage/MainPage.jsx
@@ -0,0 +1,8 @@
+import React, {useState, useEffect} from 'react'
+
+export default function MainPage(){
+
+    return (
+        <p>I am Main Page</p>
+    )
+}
