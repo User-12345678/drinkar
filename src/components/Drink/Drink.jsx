b870a1d6d56946cbcafc6fac679b515f4eb3662e
User-123456789 <davidkloth@yahoo.se>
2022-02-13 23:33:50 +0100

    new design

--- src/components/Drink/Drink.jsx
+++ src/components/Drink/Drink.jsx
@@ -16,8 +16,11 @@ export default function Drink(props) {
             <img src={drink.img} alt={drink.name}/>
             <Ingredients ingredient={drink.ingredients} />
             { showDescription?  <Description description={drink.description} className='description' /> : null }
+            <div className="buttonHolder">
             { !showDescription && drink.description ? <button className='drinkButton'  onClick={() => setShow(true)}> Visa mer info</button> : null}
             { showDescription ? <button className='drinkButton'  onClick={() => setShow(false)}> Visa mindre info</button> : null}
+            </div>
+  
         </div> 
     )
 }
--- src/components/Ingredients/Ingredients.jsx
+++ src/components/Ingredients/Ingredients.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./Ingretients.css"
 
 export default function Ingredients(props){
 
