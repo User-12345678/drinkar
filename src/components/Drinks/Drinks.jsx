e420c586b1f51351f9916b0870ca4ac9622077c5
Gudslamm <davidkloth@yahoo.se>
2022-02-23 18:54:07 +0100

    fix

--- src/components/Drinks/Drinks.jsx
+++ src/components/Drinks/Drinks.jsx
@@ -4,6 +4,7 @@ import { loadDrinks, isLoading, selectFilterDrinks } from '../../store/drinkSlic
 import "./Drinks.css"
 import Drink from '../Drink/Drink'
 import MyLoader from '../../features/Skeleton/Skeleton';
+import { Spring, animated } from 'react-spring'
 
 export default function Drinks(){
 
@@ -18,33 +19,30 @@ export default function Drinks(){
 
     if(isDrinkLoading){
         return (
-            <div className='drinks'>
-                <MyLoader />
-                <MyLoader />
-                <MyLoader />
-                <MyLoader />
-                <MyLoader />
-                <MyLoader />
-                <MyLoader />
-                <MyLoader />
-                <MyLoader />
-                <MyLoader />
-                <MyLoader />
-                <MyLoader />
-            </div>
-           
+            <div className='empty'></div>
         )
-           
-
-      
     }
 
     return (
-        <div className='drinks'>
-                {filterDrinks?.map(drink => {
-                 return <Drink drink={drink}/>
+  
+            <Spring 
+             from={{opacity: 0}}
+             to={{opacity: 1}}
+             config={{duration: 1500}}>
+                 {styles => (
+                     <animated.div className='drinks' style={styles}>
+                             {filterDrinks?.map(drink => {
+                            return <Drink drink={drink}/>
              })}
-        </div>
+           
+                     </animated.div>
+                 )}
+
+            </Spring>
+        
+        
+          
+     
        
     )
 
